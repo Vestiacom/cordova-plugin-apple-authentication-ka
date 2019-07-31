bc66ca43c4948e13120cf147698188a6db8bf312
Griffin Ledingham <gcledingham@gmail.com>
2019-07-30 19:12:15 -0700

    Tidy whitespace

--- www/AppleAuthentication.js
+++ www/AppleAuthentication.js
@@ -2,7 +2,6 @@ var exec = require("cordova/exec");
 var PLUGIN_NAME = "AppleAuthentication";
 
 module.exports = {
-
     getToken: function(success, error) {
         exec(success, error, PLUGIN_NAME, "getToken", []);
     }
